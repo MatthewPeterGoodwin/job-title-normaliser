d3b9a256b6fe43b92538d727b7327350898ad000
MatthewPeterGoodwin <matthewpgoodwin@sky.com>
2025-01-23 16:44:18 +0000

    Initial commit

---
+++ src/main/java/com/mpg/normaliser/Normaliser.java
@@ -0,0 +1,11 @@
+package com.mpg.normaliser;
+
+/**
+ * Class to normalise job titles
+ */
+public class Normaliser {
+
+    public String normalise(String inputTitle){
+        return "";
+    }
+}
---
+++ src/test/java/com/mpg/normaliser/NormaliserTest.java
@@ -0,0 +1,18 @@
+package com.mpg.normaliser;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NormaliserTest {
+
+    @Test
+    public void testNormalise() {
+        Normaliser normaliser = new Normaliser();
+
+        assertEquals("Software engineer", normaliser.normalise("Java engineer"));
+        assertEquals("Software engineer", normaliser.normalise("C# engineer"));
+        assertEquals("Accountant", normaliser.normalise("Accountant"));
+        assertEquals("Accountant", normaliser.normalise("Chief Accountant"));
+    }
+
+}
