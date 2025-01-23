791ef2b9c559bfff4b2e3e53e42a22461a9d1573
MatthewPeterGoodwin <matthewpgoodwin@sky.com>
2025-01-23 21:19:56 +0000

    Further tests added

--- src/test/java/com/mpg/normaliser/NormaliserTest.java
+++ src/test/java/com/mpg/normaliser/NormaliserTest.java
@@ -1,18 +1,52 @@
 package com.mpg.normaliser;
-import org.junit.Test;
 
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class NormaliserTest {
 
-    @Test
-    public void testNormalise() {
-        Normaliser normaliser = new Normaliser();
+    Normaliser normaliser = new Normaliser();
 
+    @Test
+    public void testNormaliseWithExactMatches() {
+
+        assertEquals("Software engineer", normaliser.normalise("Software engineer"));
+        assertEquals("Architect", normaliser.normalise("Architect"));
+        assertEquals("Quantity surveyor", normaliser.normalise("Quantity surveyor"));
+        assertEquals("Accountant", normaliser.normalise("Accountant"));
+    }
+
+    @Test
+    public void testNormaliseWithSampleCode() {
         assertEquals("Software engineer", normaliser.normalise("Java engineer"));
         assertEquals("Software engineer", normaliser.normalise("C# engineer"));
-        assertEquals("Accountant", normaliser.normalise("Accountant"));
         assertEquals("Accountant", normaliser.normalise("Chief Accountant"));
+        assertEquals("Architect", normaliser.normalise("Building Architect"));
+    }
+
+    @Test
+    public void testNormaliseWithCaseInsensitivity() {
+        assertEquals("Software engineer", normaliser.normalise("SOFTWARE ENGINEER"));
+        assertEquals("Architect", normaliser.normalise("architect"));
+        assertEquals("Quantity surveyor", normaliser.normalise("QUANTITY SURVEYOR"));
+        assertEquals("Accountant", normaliser.normalise("aCcOuNtAnT"));
+    }
+
+    @Test
+    public void testNormaliseWithNullAndEmpty() {
+        assertEquals("", normaliser.normalise(null));
+        assertEquals("", normaliser.normalise(""));
+        assertEquals("", normaliser.normalise("   "));
+    }
+
+
+    @Test
+    public void testCalculateDistance() {
+        assertEquals(0, normaliser.calculateDistance("test", "test"));
+        assertEquals(1, normaliser.calculateDistance("test", "tests"));
+        assertEquals(3, normaliser.calculateDistance("kitten", "sitting"));
+        assertEquals(2, normaliser.calculateDistance("flaw", "lawn"));
+        assertEquals(2, normaliser.calculateDistance("distance", "instance"));
     }
 
 }
